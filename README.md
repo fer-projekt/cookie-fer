e0a7c35002089568bf822f15f4eb377fdda38eab
Hanzlo <hanzlo89@gmail.com>
2024-02-20 15:35:39 +0100

    update language support, pixel code support

--- README.md
+++ README.md
@@ -1,33 +1,64 @@
-# FerCookieBot: Consent Management ( Google Consent Mode v2 )
-FerCookieBot is a JavaScript-based solution designed to help website owners manage user consents for cookies and tracking technologies in compliance with data protection regulations like GDPR and CCPA. It integrates seamlessly with Google Consent Mode v2, allowing for dynamic consent handling and ensuring a respectful and legal approach to user data.
+# FerCookieBot: Enhanced Consent Management for Google Consent v2 and Facebook Tracking
+FerCookieBot is an advanced JavaScript solution designed for website owners to manage user consents for cookies and tracking technologies, ensuring compliance with data protection regulations like GDPR and CCPA. It now seamlessly integrates with both Google Consent Mode v2 and Facebook Pixel, offering dynamic consent handling and a respectful, legal approach to user data across major advertising platforms.
 
 ## Features
+
 **Consent Dialog:** A customizable dialog prompts users for their cookie preferences, enhancing transparency and control over personal data processing.
 
 **Local Storage:** User preferences are stored locally, ensuring their choices persist across sessions and visits, thereby improving the user experience by eliminating repetitive consent prompts.
 
 **Dynamic Preference Management:** Users can modify their consent choices at any time, providing ongoing compliance and flexibility.
 
-**Google Tag Manager Integration**: Seamlessly integrates with Google Tag Manager (`gtag.js`), enabling or disabling specific tracking functionalities based on the obtained consents, ensuring compliance while maintaining the utility of analytics and advertising cookies.
+**Google Tag Manager and Facebook Pixel Integration:**: Provides seamless integration with Google Tag Manager (gtag.js) and Facebook Pixel, enabling or disabling specific tracking functionalities based on obtained consents. This ensures compliance while maintaining the utility of analytics and advertising cookies.
+
+**Multi-Language Support**: Dynamically creates consent dialogs based on the user's browser language, enhancing accessibility and user experience for a global audience.
 
 **Security and Isolation:** Adheres to domain-specific storage rules, safeguarding user preferences and ensuring data isolation in line with web security standards.
 
 **To integrate FerCookieBot into your website, follow these steps:**
 
 ## Installation & Initialization
+
 Include the FerCookieBot css in your project.
 
     <link  rel="stylesheet"  href="css/fer-cookiebot.css">
 
 Include the FerCookieBot script in your project.
 
-Create an instance of the FerCookieBot class with your Google Tag ID:
+Create an instance of the FerCookieBot class with your Google Tag ID and Facebook Pixel ID (Pixel and options are optional):
 
     import FerCookieBot from './path/to/fer-cookiebot.js';
-    /* ADD YOUR GTAG ID, OPTIONS - CREATES GTAG/GA4 TAG AND INITIALIZE COOKIES */
-    const  cookieBot  =  new  FerCookieBot('YOUR_GOOGLE_TAG_ID', cookieBotOptions);
+    const cookieBotOptions = {
+        // Your customization options / translations
+    };
+    /* ADD YOUR GTAG ID, PIXEL_ID, OPTIONS - CREATES GTAG/GA4 TAG, PIXEL TAG AND INITIALIZE COOKIES */
+    const cookieBot = new FerCookieBot('YOUR_GOOGLE_TAG_ID');
+    /* const cookieBot = new FerCookieBot('YOUR_GOOGLE_TAG_ID', 'YOUR_FACEBOOK_PIXEL_ID', cookieBotOptions); */
 
-## Customization
+
+
+## Multi-Language Support
+
+FerCookieBot now offers comprehensive multi-language support, enabling you to present the consent dialog in the user's preferred language. This feature enhances the user experience for a global audience by providing localized consent information, ensuring clarity and improving compliance with international privacy regulations.
+
+FerCookieBot supports the following languages out of the box:
+
+**Supported Languages:**
+
+ - English (en) 
+ - German (de) 
+ - Italian (it) 
+ - Russian (ru) 
+ - Polish (pl)
+ - Slovenian (sl) 
+ - Czech (cs) 
+ - Dutch (nl) 
+ - Icelandic (is) 
+ - French (fr)
+ - Croatian (hr)
+
+**Customizing Language Content** 
+
 FerCookieBot allows for  customization through options passed at initialization. This enables you  to specify the exact text and titles for various cookie categories and consent information.
 
     const cookieBotOptions = {
@@ -43,6 +74,7 @@ FerCookieBot allows for  customization through options passed at initialization.
     };
 
 ## Preference Button
+
 Implement a button allowing users to change their cookie preferences at any time:
 
     <button id="changeCookieBotPreferences">Change Cookie Preferences</button>
@@ -50,4 +82,5 @@ Implement a button allowing users to change their cookie preferences at any time
 Remember to replace placeholders (like path/to/fer-cookiebot.js, YOUR_GOOGLE_TAG_ID, and links to documentation or license files) with actual paths and IDs relevant to your project. 
 
 ## License
+
 This project is licensed under the MIT License - see the LICENSE file for details.
--- js/fer-cookiebot.js
+++ js/fer-cookiebot.js
@@ -1,23 +1,15 @@
 /* VERIFY CONSENT https://tagassistant.google.com */
 
 class FerCookieBot {
-    constructor(googleTagId, options = {}) {
+    
+    constructor(googleTagId, facebookPixelId,  options = {}) {
         this.googleTagId = googleTagId;
+        this.facebookPixelId = facebookPixelId || null;
         this.dialogId = 'consentDialog';
-
-        this.options = {
-            title: "Your Consent Title",
-            necessary_cookies_title: "Necessary Cookies",
-            ad_storage_title: "Advertising Cookies",
-            ad_user_data_title: "Use of Advertising Data",
-            ad_personalization_title: "Ad Personalization",
-            analytics_storage_title: "Analytics Cookies",
-            button_title: "Save Settings",
-            consent_text: "This website uses cookies. We use cookies to personalize content and ads, provide social media features, and analyze our traffic.",
-            consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Learn More</a>",
-            ...options
-        };
-
+        this.options = options;
+        this.translations = this.defineTranslations();
+        this.language = document.documentElement.lang || "en"; // Default to English
+        this.translatedOptions = this.translations[this.language] || this.translations["en"]; // Fallback to English
         this.loadGTagScript().then(() => {
             this.initDataLayer();
             this.initializeConsentMode();
@@ -50,6 +42,34 @@ class FerCookieBot {
         });
     }
 
+    addFacebookPixel() {
+        // console.log('Adding Facebook Pixel...');
+        if (!this.facebookPixelId) return;
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', this.facebookPixelId); // Replace 'YOUR_PIXEL_ID' with your actual Facebook Pixel ID
+        fbq('track', 'PageView');
+        this.addFacebookPixelNoscript();
+    }
+
+    addFacebookPixelNoscript() {
+        const noscript = document.createElement('noscript');
+        const img = document.createElement('img');
+        img.height = 1;
+        img.width = 1;
+        img.style.display = 'none';
+        img.src = `https://www.facebook.com/tr?id=${this.facebookPixelId}&ev=PageView&noscript=1`;
+        noscript.appendChild(img);
+        // Append the noscript tag to the body or a specific element within your page.
+        document.body.insertBefore(noscript, document.body.firstChild);
+    }
+
     initializeConsentMode() {
         this.gtag('js', new Date());
         this.gtag('consent', 'default', {
@@ -69,7 +89,6 @@ class FerCookieBot {
             'ad_personalization': adPersonalization ? 'granted' : 'denied',
             'analytics_storage': analyticsStorage ? 'granted' : 'denied',
         });
-
         localStorage.setItem('consentSettings', JSON.stringify({
             adStorage,
             adUserData,
@@ -79,46 +98,40 @@ class FerCookieBot {
     }
 
     createConsentDialog() {
-        
-         // Create the dialog element
+        // Create the dialog element
         const dialog = document.createElement('dialog');
         dialog.id = 'consentDialog';
-        
-        // Append the dialog to the document body
         document.body.appendChild(dialog);
-
-        // Set title
+        // Set the title from translated options
         const titleDiv = document.createElement('div');
         titleDiv.id = 'consent_title';
-        titleDiv.innerText = this.options.title;
+        titleDiv.innerText = this.translatedOptions.title;
         dialog.appendChild(titleDiv);
-
-        // Create and append checkboxes and labels
-        const items = [{
+        // Dynamically create and append checkbox items with labels from translated options
+        const consentItems = [{
                 id: 'necessary_cookies',
-                text: this.options.necessary_cookies_title,
+                text: this.translatedOptions.necessary_cookies_title,
                 disabled: true,
                 checked: true
             },
             {
                 id: 'ad_storage',
-                text: this.options.ad_storage_title
+                text: this.translatedOptions.ad_storage_title
             },
             {
                 id: 'ad_user_data',
-                text: this.options.ad_user_data_title
+                text: this.translatedOptions.ad_user_data_title
             },
             {
                 id: 'ad_personalization',
-                text: this.options.ad_personalization_title
+                text: this.translatedOptions.ad_personalization_title
             },
             {
                 id: 'analytics_storage',
-                text: this.options.analytics_storage_title
+                text: this.translatedOptions.analytics_storage_title
             }
         ];
-
-        items.forEach(item => {
+        consentItems.forEach(item => {
             const label = document.createElement('label');
             label.className = 'consent-item';
 
@@ -132,20 +145,19 @@ class FerCookieBot {
 
             label.appendChild(input);
             label.appendChild(textNode);
+
             dialog.appendChild(label);
         });
-
-        // Set consent text
+        // Set the consent text from translated options
         const consentTextDiv = document.createElement('div');
         consentTextDiv.id = 'consent_text';
-        consentTextDiv.innerHTML = this.options.consent_text + ' ' + this.options.consent_link;
+        consentTextDiv.innerHTML = this.translatedOptions.consent_text + ' ' + this.translatedOptions.consent_link;
         dialog.appendChild(consentTextDiv);
-
-        // Save preferences button
+        // Create and append the save preferences button
         const saveButton = document.createElement('button');
         saveButton.id = 'saveCookieBotPreferences';
         saveButton.className = 'consent-button';
-        saveButton.innerText = this.options.button_title;
+        saveButton.innerText = this.translatedOptions.button_title;
         dialog.appendChild(saveButton);
     }
 
@@ -157,13 +169,16 @@ class FerCookieBot {
             document.getElementById('changeCookieBotPreferences').addEventListener('click', () => {
                 this.openDialog();
             });
-            document.getElementById('consent_title').innerText = this.options.title;
         }
+
         if (consentSettings) {
             document.getElementById('ad_storage').checked = consentSettings.adStorage;
             document.getElementById('ad_user_data').checked = consentSettings.adUserData;
             document.getElementById('ad_personalization').checked = consentSettings.adPersonalization;
             document.getElementById('analytics_storage').checked = consentSettings.analyticsStorage;
+        }
+        if (consentSettings.adStorage == true && consentSettings.adUserData == true) {
+            this.addFacebookPixel();
         }
     }
 
@@ -206,6 +221,133 @@ class FerCookieBot {
         localStorage.setItem('dialogState', 'closed');
         this.closeDialog();
     }
-}
 
+    defineTranslations() {
+        return {
+            // Define your translations for each language here
+            hr: {
+                title: "Vaš naslov pristanka",
+                necessary_cookies_title: "Nužni kolačići",
+                ad_storage_title: "Oglašavački kolačići",
+                ad_user_data_title: "Korištenje podataka o oglašavanju",
+                ad_personalization_title: "Personalizacija oglasa",
+                analytics_storage_title: "Kolačići analitike",
+                button_title: "Spremi postavke",
+                consent_text: "Ova web-stranica koristi kolačiće. Kolačiće upotrebljavamo kako bismo personalizirali sadržaj i oglase, omogućili značajke društvenih medija i analizirali promet.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Opširnije</a>",
+            },
+            en: {
+                title: "Your Consent Title",
+                necessary_cookies_title: "Necessary Cookies",
+                ad_storage_title: "Advertising Cookies",
+                ad_user_data_title: "Use of Advertising Data",
+                ad_personalization_title: "Ad Personalization",
+                analytics_storage_title: "Analytics Cookies",
+                button_title: "Save Settings",
+                consent_text: "This website uses cookies. We use cookies to personalize content and ads, provide social media features, and analyze our traffic.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Learn More</a>",
+            },
+            de: {
+                title: "Ihre Zustimmung",
+                necessary_cookies_title: "Notwendige Cookies",
+                ad_storage_title: "Werbungs-Cookies",
+                ad_user_data_title: "Nutzung von Werbedaten",
+                ad_personalization_title: "Personalisierung der Werbung",
+                analytics_storage_title: "Analyse-Cookies",
+                button_title: "Einstellungen speichern",
+                consent_text: "Diese Webseite verwendet Cookies. Wir verwenden Cookies, um Inhalte und Anzeigen zu personalisieren, Funktionen für soziale Medien anbieten zu können und den Verkehr zu analysieren.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Mehr erfahren</a>",
+            },
+            it: {
+                title: "Il tuo consenso",
+                necessary_cookies_title: "Cookie necessari",
+                ad_storage_title: "Cookie pubblicitari",
+                ad_user_data_title: "Utilizzo dei dati pubblicitari",
+                ad_personalization_title: "Personalizzazione degli annunci",
+                analytics_storage_title: "Cookie analitici",
+                button_title: "Salva impostazioni",
+                consent_text: "Questo sito utilizza i cookie. Utilizziamo i cookie per personalizzare contenuti e annunci, fornire funzionalità dei social media e analizzare il nostro traffico.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Scopri di più</a>",
+            },
+            ru: {
+                title: "Ваше согласие",
+                necessary_cookies_title: "Необходимые куки",
+                ad_storage_title: "Куки для рекламы",
+                ad_user_data_title: "Использование данных для рекламы",
+                ad_personalization_title: "Персонализация рекламы",
+                analytics_storage_title: "Аналитические куки",
+                button_title: "Сохранить настройки",
+                consent_text: "Этот сайт использует куки. Мы используем куки для персонализации контента и рекламы, предоставления социальных медиа функций и анализа нашего трафика.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Узнать больше</a>",
+            },
+            pl: {
+                title: "Twoja zgoda",
+                necessary_cookies_title: "Niezbędne ciasteczka",
+                ad_storage_title: "Ciasteczka reklamowe",
+                ad_user_data_title: "Używanie danych reklamowych",
+                ad_personalization_title: "Personalizacja reklam",
+                analytics_storage_title: "Ciasteczka analityczne",
+                button_title: "Zapisz ustawienia",
+                consent_text: "Ta strona używa ciasteczek. Używamy ciasteczek do personalizacji treści i reklam, oferowania funkcji mediów społecznościowych i analizowania ruchu na stronie.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Dowiedz się więcej</a>",
+            },
+            sl: {
+                title: "Vaše soglasje",
+                necessary_cookies_title: "Nujni piškotki",
+                ad_storage_title: "Oglaševalski piškotki",
+                ad_user_data_title: "Uporaba oglaševalskih podatkov",
+                ad_personalization_title: "Personalizacija oglasov",
+                analytics_storage_title: "Analitični piškotki",
+                button_title: "Shrani nastavitve",
+                consent_text: "Ta spletna stran uporablja piškotke. Piškotke uporabljamo za personalizacijo vsebine in oglasov, omogočanje funkcij družbenih omrežij in analizo prometa.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Več informacij</a>",
+            },
+            cs: {
+                title: "Váš souhlas",
+                necessary_cookies_title: "Nezbytné cookies",
+                ad_storage_title: "Reklamní cookies",
+                ad_user_data_title: "Použití reklamních dat",
+                ad_personalization_title: "Personalizace reklam",
+                analytics_storage_title: "Analytické cookies",
+                button_title: "Uložit nastavení",
+                consent_text: "Tato webová stránka používá cookies. Používáme cookies k personalizaci obsahu a reklam, k poskytování funkcí sociálních médií a k analýze našeho provozu.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Více informací</a>",
+            },
+            nl: {
+                title: "Uw toestemming",
+                necessary_cookies_title: "Noodzakelijke cookies",
+                ad_storage_title: "Advertentiecookies",
+                ad_user_data_title: "Gebruik van advertentiegegevens",
+                ad_personalization_title: "Advertentiepersonalisatie",
+                analytics_storage_title: "Analytische cookies",
+                button_title: "Instellingen opslaan",
+                consent_text: "Deze website gebruikt cookies. We gebruiken cookies om content en advertenties te personaliseren, om functies voor sociale media te bieden en om ons verkeer te analyseren.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Meer leren</a>",
+            },
+            is: {
+                title: "Þitt samþykki",
+                necessary_cookies_title: "Nauðsynlegar kökur",
+                ad_storage_title: "Auglýsingakökur",
+                ad_user_data_title: "Notkun auglýsingagagna",
+                ad_personalization_title: "Sérsniðnar auglýsingar",
+                analytics_storage_title: "Greiningarkökur",
+                button_title: "Vista stillingar",
+                consent_text: "Þessi vefsíða notar kökur. Við notum kökur til að sérsníða efni og auglýsingar, bjóða upp á samfélagsmiðlaeiginleika og greina umferð okkar.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>Lærðu meira</a>",
+            },
+            fr: {
+                title: "Votre consentement",
+                necessary_cookies_title: "Cookies nécessaires",
+                ad_storage_title: "Cookies publicitaires",
+                ad_user_data_title: "Utilisation des données publicitaires",
+                ad_personalization_title: "Personnalisation des annonces",
+                analytics_storage_title: "Cookies analytiques",
+                button_title: "Sauvegarder les paramètres",
+                consent_text: "Ce site utilise des cookies. Nous utilisons des cookies pour personnaliser le contenu et les annonces, offrir des fonctionnalités de médias sociaux et analyser notre trafic.",
+                consent_link: "<a href='https://policies.google.com/privacy' target='_blank'>En savoir plus</a>",
+            },
+            // Add other languages...
+        };
+    }
+}
 export default FerCookieBot;
